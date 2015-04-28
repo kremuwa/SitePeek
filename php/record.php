36ecbb89ce38a6a126b6d25155060d90ae4d4122
kremuwa <kremuwa@gmail.com>
2015-04-28 18:49:06 +0200

    Pierwszy commit - zapisywanie ramek do bazy danych przy okazji każdego eventu mousemove.

---
+++ php/record.php
@@ -0,0 +1,24 @@
+<?php
+
+    error_reporting(-1);
+
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=peek_db', 'peek-user', 'B7FpQbpD6auDK2mr', array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING /* DEBUG */
+        ));
+
+        $dbh->beginTransaction();
+
+        $stmt = $dbh->prepare("INSERT INTO frames (mouseX, mouseY) VALUES (?, ?)");
+        $stmt->bindParam(1, $_POST['mouseX']);
+        $stmt->bindParam(2, $_POST['mouseY']);
+
+        $stmt->execute();
+
+        $dbh->commit();
+
+    } catch (PDOException $e) {
+        print "Error: " . $e->getMessage() . '<br />';
+        die();
+    }
