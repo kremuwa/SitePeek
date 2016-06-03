21507bb619b6331c9fe63086db55d9fc99e757c4
kremuwa <kremuwa@gmail.com>
2016-06-03 19:14:52 +0200

    Back to original texts.

--- public_html/play.php
+++ public_html/play.php
@@ -8,200 +8,159 @@
 
 ?>
 
-    <div id="stage1">
+<div id="stage1">
 
-       <div class="info">
+    <div class="info">
 
-            <h1>
-                Watch Your pals with a hidden camera
-            </h1>
+        <h1>
+            Watch Your pals with a hidden camera
+        </h1>
 
-            <p>
-                TheNetSpy.com lets You make some fun of Your friends, by showing You what exactly
-                they are doing on a wacky news website - without them knowing!
-            </p>
+        <p>
+            TheNetSpy.com lets You make some fun of Your friends, by showing You what exactly
+            they are doing on a wacky news website - without them knowing!
+        </p>
 
-            <p>
-                You can get the idea by looking on the background ;-)
-            </p>
+        <p>
+            You can get the idea by looking on the background ;-)
+        </p>
 
-            <p>
-                You will see which article they choose, what they focus on, which comment they agree with and
-                what they have to say.
-            </p>
+        <p>
+            You will see which article they choose, what they focus on, which comment they agree with and
+            what they have to say.
+        </p>
 
-            <a id="start" href="#">Let's go!</a>
+        <a id="start" href="#">Let's go!</a>
 
+    </div>
+
+    <div id="preview-wrapper">
+        <iframe id="preview-frame"></iframe>
+        <div id="preview-panzoom-layer"></div>
+    </div>
+
+</div>
+
+<div id="stage2">
+    <iframe id="preparation-frame" src="wp"></iframe>
+    <div id="generateWrapper">
+        <div style="font-size: 0.8em">Choose a starting page.</div>
+        <div style="font-size: 1em"><strong>THEN</strong> click:</div>
+        <a id="generate" href="#">
+            Generate
+        </a>
+    </div>
+    <div id="dialog1" title="Choose a starting page">
+        <p>Behind this dialog You can see a wacky news site that
+            You will use to watch Your friend's activity.</p>
+        <p>Your pal might
+            be a bit surprised though, if You just sent them
+            a link to the homepage of a funny site, right? So, we suggest that You choose some interesting article as a starting point.</p>
+        <p><b>Choose a starting page for Your friend, then click "Generate".</b></p>
+    </div>
+</div>
+
+<div id="stage3">
+    <div id="generating">Generating Your link...</div>
+</div>
+
+<div id="stage4">
+
+    <div id="linkbox">
+
+        <p>
+            Copy Your generated link and think of the best
+            way of sending it to Your friend (so that they don't
+            get suspicious!)<br/>
+        </p>
+
+        <p>
+            To copy, press CTRL+C or touch & hold:
+        </p>
+
+        <div id="copybox1">
+            <label>
+                <input type="text" />
+            </label>
         </div>
 
-        <div id="preview-wrapper">
-            <iframe id="preview-frame"></iframe>
-            <div id="preview-panzoom-layer"></div>
+        <p>
+            You can also use the buttons below:
+        </p>
+
+        <a href="#" class="whatsapp-send-btn" style="display:none">Send using WhatsApp</a>
+        <a href="#" class="fb-send-btn" style="display:none">Send using Messenger</a>
+        <a href="#" class="fb-share-btn">Share on Facebook</a>
+
+        <p>
+            <img class="loading" src="img/loading.gif" />
+            <strong>Waiting for Your friend to click the link...</strong>
+        </p>
+
+        <div id="dialog2" title="Heads up!">
+            <p>
+                Our site is meant just for fun, not for spying. When You
+                finish watching, Your friend will be notified about the joke!
+                So do it only if You know they won't be too mad at You! ;)
+            </p>
         </div>
 
     </div>
 
-    <div id="stage2">
-        <iframe id="preparation-frame" src="wp"></iframe>
-        <div id="generateWrapper">
-            <div style="font-size: 0.8em">Choose a starting page.</div>
-            <div style="font-size: 1em"><strong>THEN</strong> click:</div>
-            <a id="generate" href="#">
-                Generate
-            </a>
-        </div>
-        <div id="dialog1" title="Choose a starting page">
-            <p>Behind this dialog You can see a wacky news site that
-                You will use to watch Your friend's activity.</p>
-            <p>Your pal might
-                be a bit surprised though, if You just sent them
-                a link to the homepage of a funny site, right? So, we suggest that You choose some interesting article as a starting point.</p>
-            <p><b>Choose a starting page for Your friend, then click "Generate".</b></p>
-        </div>
+</div>
+
+<div id="stage5">
+    <div id="counter">N/A</div>
+</div>
+
+<div id="stage6">
+    <div id="menu">
+        <a class="again" href=".">Generate another link</a>
+        <span id="zoominfo">Pan and zoom using mouse or your fingers</span>
     </div>
-
-    <div id="stage3">
-        <div id="generating">Generating Your link...</div>
+    <div id="wrapper">
+        <iframe id="playing-frame"></iframe>
+        <div id="panzoom-layer"></div>
     </div>
+    <div id="message-box"> <!-- can be generalized if needed -->
+        A second person clicked Your link, while the first one is still
+        recorded. Two people can't be recorded at the same time.
+        The second person was redirected to the main page of TheNetSpy.com.
+    </div>
+</div>
 
-    <div id="stage4">
+<div id="stage7">
 
-        <!--
+    <div id="stage7content">
 
-        <div id="linkbox">
+        <p>
+            The user has left the site. As we told You before, they were notified about Your joke.
+            We hope You'll survive until tomorrow :)
+        </p>
 
-            <p>
-                Copy Your generated link and think of the best
-                way of sending it to Your friend (so that they don't
-                get suspicious!)<br/>
-            </p>
+        <p>
+            You can now send the link to another person:
+        </p>
 
-            <p>
-                To copy, press CTRL+C or touch & hold:
-            </p>
-
-            <div id="copybox1">
-                <label>
-                    <input type="text" />
-                </label>
-            </div>
-
-            <p>
-                You can also use the buttons below:
-            </p>
-
-            <a href="#" class="whatsapp-send-btn" style="display:none">Send using WhatsApp</a>
-			<a href="#" class="fb-send-btn" style="display:none">Send using Messenger</a>
-            <a href="#" class="fb-share-btn">Share on Facebook</a>
-
-            <p>
-                <img class="loading" src="img/loading.gif" />
-                <strong>Waiting for Your friend to click the link...</strong>
-            </p>
-
-            <div id="dialog2" title="Heads up!">
-                <p>
-                    Our site is meant just for fun, not for spying. When You
-                    finish watching, Your friend will be notified about the joke!
-                    So do it only if You know they won't be too mad at You! ;)
-                </p>
-            </div>
-
+        <div id="copybox2">
+            <label>
+                <input type="text" />
+            </label>
         </div>
 
-        -->
+        <a href="whatsapp://send" data-text="Take a look:" data-href="" class="wa_btn wa_btn_l" style="display:none">Send using WhatsApp</a>
+        <a href="#" class="fb-send-btn" style="display:none">Send with Messenger</a>
+        <a href="#" class="fb-share-btn">Share on Facebook</a>
 
-        <div id="linkbox">
+        <p>
+            ... or generate a new link by <a class="again" href=".">clicking here</a>
+        </p>
 
-            <p>
-                Copy Your generated link and send it to the person with whom you want to test your site.<br/>
-            </p>
-
-            <p>
-                To copy, press CTRL+C or touch & hold:
-            </p>
-
-            <div id="copybox1">
-                <label>
-                    <input type="text" />
-                </label>
-            </div>
-
-            <p>
-                You can also use the buttons below:
-            </p>
-
-            <a href="#" class="whatsapp-send-btn" style="display:none">Send using WhatsApp</a>
-			<a href="#" class="fb-send-btn" style="display:none">Send using Messenger</a>
-            <a href="#" class="fb-share-btn">Share on Facebook</a>
-
-            <p>
-                <img class="loading" src="img/loading.gif" />
-                <strong>Waiting for your testing target to click the link...</strong>
-            </p>
-
-            <div id="dialog2" title="Heads up!">
-                <p>
-                    There will be a delay of 10 seconds between what happens and what you see.
-                </p>
-            </div>
-
-        </div>
+        <p>
+            <img class="loading" src="img/loading.gif" />
+            <strong>Waiting for Your friend to click the link...</strong>
+        </p>
 
     </div>
 
-    <div id="stage5">
-        <div id="counter">N/A</div>
-    </div>
-
-    <div id="stage6">
-        <div id="menu">
-            <a class="again" href=".">Generate another link</a>
-            <span id="zoominfo">Pan and zoom using mouse or your fingers</span>
-        </div>
-        <div id="wrapper">
-            <iframe id="playing-frame"></iframe>
-            <div id="panzoom-layer"></div>
-        </div>
-        <div id="message-box"> <!-- can be generalized if needed -->
-            A second person clicked Your link, while the first one is still
-            recorded. Two people can't be recorded at the same time.
-            The second person was redirected to the main page of TheNetSpy.com.
-        </div>
-    </div>
-
-    <div id="stage7">
-
-        <div id="stage7content">
-
-            <p>
-                The user has left the site. As we told You before, they were notified about Your joke.
-                We hope You'll survive until tomorrow :)
-            </p>
-
-            <p>
-                You can now send the link to another person:
-            </p>
-
-            <div id="copybox2">
-                <label>
-                    <input type="text" />
-                </label>
-            </div>
-
-            <a href="whatsapp://send" data-text="Take a look:" data-href="" class="wa_btn wa_btn_l" style="display:none">Send using WhatsApp</a>
-            <a href="#" class="fb-send-btn" style="display:none">Send with Messenger</a>
-            <a href="#" class="fb-share-btn">Share on Facebook</a>
-
-            <p>
-                ... or generate a new link by <a class="again" href=".">clicking here</a>
-            </p>
-
-            <p>
-                <img class="loading" src="img/loading.gif" />
-                <strong>Waiting for Your friend to click the link...</strong>
-            </p>
-
-        </div>
-
-    </div>
+</div>
