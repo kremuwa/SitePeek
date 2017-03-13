72d519f7f5e0b68b784f6756371b2b8f582a0ca4
kremuwa <kremuwa@gmail.com>
2017-03-13 18:53:34 +0100

    Deleted code related to preview-frame, as we don't have one anymore.
    Disabled pointer-events for player-frame so that the watching person cannot interfere with the playback.
    
    Deleted obsolete server directives from .htaccess.
    
    Deleted unnecessary files coming form HTML5 Boilerplate.

--- public_html/ajax/getFrames.php
+++ public_html/ajax/getFrames.php
@@ -1,43 +1,29 @@
 <?php
 
-    header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 
-    // main code
+// main code
 
-    error_reporting(-1); //DEBUG
+error_reporting(-1); //DEBUG
 
-    $msecToLookBeforeLastAvailableFrame = 4000; // how long before the time of last available frame should we start
-    $msecToLookBackForData = 10000; // how many msecs to look back from current time for frames
+$msecToLookBeforeLastAvailableFrame = 4000; // how long before the time of last available frame should we start
+$msecToLookBackForData = 10000; // how many msecs to look back from current time for frames
 
-    try {
-        $dbh = new PDO('mysql:host=localhost;dbname=peek_db', 'peek-user', 'B7FpQbpD6auDK2mr', array(
-            PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING /* DEBUG */
-        ));
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=peek_db', 'peek-user', 'B7FpQbpD6auDK2mr', array(
+        PDO::ATTR_PERSISTENT => true,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING /* DEBUG */
+    ));
 
-        // if we're asking for data from preview-frame
+    if ($_POST['lastTimestamp'] == 0) {
 
-        if($_POST['testspaceId'] == 'preview-frame') {
+        // (I hope that in case of null in the subquery, the query will return nothing)
+        // in subquery we look for the latest data, unless it's older than
+        // $msecToLookBackForData and then we choose all frames that are more recent than the
+        // found frame time - $msecToLookBeforeLastAvailableFrame
+        // ASes provide security through obscurity, changing client-side names of properties
 
-            $sql = "SELECT
-                       frameType AS type, frameTimestamp AS timestamp, frameMouseX AS mouseX, frameMouseY AS mouseY,
-                       frameTarget AS target, frameText AS text, frameCaret AS caret, frameScrollTop AS scrollTop,
-                       frameWidth AS width, frameHeight AS height, frameHref AS href
-                    FROM frames
-                    WHERE testspaceId = ?";
-
-            $stmt = $dbh->prepare($sql);
-            $stmt->execute(array($_POST['testspaceId']));
-
-        } else if ($_POST['lastTimestamp'] == 0) {
-
-            // (I hope that in case of null in the subquery, the query will return nothing)
-            // in subquery we look for the latest data, unless it's older than
-            // $msecToLookBackForData and then we choose all frames that are more recent than the
-            // found frame time - $msecToLookBeforeLastAvailableFrame
-            // ASes provide security through obscurity, changing client-side names of properties
-
-            $sql = "SELECT
+        $sql = "SELECT
                        frameType AS type, frameTimestamp AS timestamp, frameMouseX AS mouseX, frameMouseY AS mouseY,
                        frameTarget AS target, frameText AS text, frameCaret AS caret, frameScrollTop AS scrollTop,
                        frameWidth AS width, frameHeight AS height, frameHref AS href
@@ -48,45 +34,42 @@
                       WHERE testspaceId = ? AND frameTimestamp > UNIX_TIMESTAMP(NOW()) * 1000 - $msecToLookBackForData
                     ) - $msecToLookBeforeLastAvailableFrame";
 
-            $stmt = $dbh->prepare($sql);
-            $stmt->execute(array($_POST['testspaceId'], $_POST['testspaceId']));
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array($_POST['testspaceId'], $_POST['testspaceId']));
 
-        } else {
+    } else {
 
-            $sql = "SELECT
+        $sql = "SELECT
                       testspaceId,/*DEUBG*/ frameType AS type, frameTimestamp AS timestamp, frameMouseX AS mouseX, frameMouseY AS mouseY,
                        frameTarget AS target, frameText AS text, frameCaret AS caret, frameScrollTop AS scrollTop,
                        frameWidth AS width, frameHeight AS height, frameHref AS href
                     FROM frames
                     WHERE testspaceId = ? AND frameTimestamp > ?";
 
-            $stmt = $dbh->prepare($sql);
-            $stmt->execute(array($_POST['testspaceId'], $_POST['lastTimestamp']));
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array($_POST['testspaceId'], $_POST['lastTimestamp']));
 
-        }
-
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        // let the client know that the result will be json (not sure if needed)
-        header('Content-Type: application/json');
-
-        // return JSON to Javascript
-        echo json_encode($result);
-
-        // removing obsolete frames (except when retrieved frames for preview-frame)
-
-        if($_POST['testspaceId'] != 'preview-frame' && $_POST['lastTimestamp'] != 0) {
-
-            $sql = "DELETE FROM frames
-                    WHERE testspaceId = ? AND frameTimestamp <= ?";
-
-            $stmt = $dbh->prepare($sql);
-            $stmt->execute(array($_POST['testspaceId'], $_POST['lastTimestamp']));
-
-        }
-
-    } catch (PDOException $e) {
-        print "Error: " . $e->getMessage() . '<br />'; // DEBUG
-        // print "Something went wrong"; // PRODUCTION
-        die();
     }
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // let the client know that the result will be json (not sure if needed)
+    header('Content-Type: application/json');
+
+    // return JSON to Javascript
+    echo json_encode($result);
+
+    // removing obsolete frames
+
+    if ($_POST['lastTimestamp'] != 0) {
+        $sql = "DELETE FROM frames
+                WHERE testspaceId = ? AND frameTimestamp <= ?";
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array($_POST['testspaceId'], $_POST['lastTimestamp']));
+    }
+
+} catch (PDOException $e) {
+    print "Error: " . $e->getMessage() . '<br />'; // DEBUG
+    // print "Something went wrong"; // PRODUCTION
+    die();
+}
