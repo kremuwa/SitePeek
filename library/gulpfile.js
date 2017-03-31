363e6a92ffe9e5131c76d8d4a2633e98f29e40aa
kremuwa <kremuwa@gmail.com>
2017-03-31 16:14:35 +0200

    Moving ajax requests out of library to script working on the sitepeek website, fixing panning, waiting for page readiness before sending msgs to it etc.

--- library/gulpfile.js
+++ library/gulpfile.js
@@ -7,14 +7,14 @@ gulp.task('dev-concat', function() {
         .pipe(gulp.dest('./build/dev'));
 });
 
-gulp.task('dev-build', function() {
+gulp.task('dev-build', ['dev-concat'], function() {
     return gulp.src(['./src/cursor.png', './src/sitepeek.css'])
         .pipe(gulp.dest('./build/dev'));
 });
 
-gulp.task('dev-publish', ['dev-concat', 'dev-build'], function() {
+gulp.task('dev-publish', ['dev-build'], function() {
     return gulp.src('./build/dev/*')
-        .pipe(gulp.dest('./../public_html/hosted-dev-library'));
+        .pipe(gulp.dest('./../public_html/hosted-library'));
 });
 
 gulp.task('prod-concat', function() {
@@ -28,10 +28,11 @@ gulp.task('prod-build', ['prod-concat'], function() {
         .pipe(gulp.dest('./build/prod'));
 });
 
-gulp.task('default', ['dev-publish', 'prod-build'], function() {
-    return gulp.watch(['./src*'], ['dev-publish', 'prod-build']);
+gulp.task('prod-publish', ['prod-build'], function() {
+    return gulp.src('./build/prod/*')
+        .pipe(gulp.dest('./../public_html/hosted-library'));
 });
 
-gulp.task('prod', ['dev-build', 'prod-build'], function() {
-    return gulp.watch(['./src/*'], ['dev-build', 'prod-build']);
+gulp.task('dev-watch', ['dev-publish', 'prod-build'], function() {
+    return gulp.watch(['./src*'], ['dev-publish', 'prod-build']);
 });
